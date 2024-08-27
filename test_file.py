2bc6022392fa76b3cfd9322c975765acc128015d
Vedant Patel <104895751+Kiraschild@users.noreply.github.com>
2024-08-27 00:17:08 +0000

    Test_file.py final version

--- test_file.py
+++ test_file.py
@@ -4,7 +4,7 @@ def true_test():
     assert add_numbers(1,2) == 3
 
 def false_test():
-    assert add_numbers(1,1) == 11
+    assert add_numbers(1,1) == 9
 
 print(true_test())
 print(false_test())
