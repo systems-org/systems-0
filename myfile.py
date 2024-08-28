4fea5d48d8141ae32178b87ed8e9fc1ba31a6c3d
Jayneel Shah <80264736+jayneel-shah18@users.noreply.github.com>
2024-08-28 18:18:17 +0000

    committ!!

--- myfile.py
+++ myfile.py
@@ -1,13 +1,6 @@
+# myfile.py
+
 def add_numbers(a, b):
     return a + b
-
-# def main():
-#     num1 = 5
-#     num2 = 10  # Error: mixing integer and string types
-#     result = add_numbers(num1, num2)  # This will cause a TypeError
-#     print("The sum is:", result)
-
-# if __name__ == "__main__":
-#     main()
 
 print("HELLO, SYSTEMS.ORG!")
--- test_file.py
+++ test_file.py
@@ -1,10 +1,9 @@
+# test_file.py
+
 from myfile import add_numbers
 
-def true_test():
-    assert add_numbers(1,2) == 3
+def test_true():
+    assert add_numbers(1, 2) == 3
 
-# def false_test():
-#     assert add_numbers(1,1) == 9
-
-print(true_test())
-# print(false_test())
+def test_false():
+    assert add_numbers(1, 1) == 2  # This should fail
