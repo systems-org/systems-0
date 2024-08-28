205cc2ccb9921d3ff4aee0d45b67c3d78cdc51e3
Jayneel Shah <80264736+jayneel-shah18@users.noreply.github.com>
2024-08-28 17:44:45 +0000

    made some more changes!!

--- test_file.py
+++ test_file.py
@@ -3,9 +3,9 @@ from myfile import add_numbers
 def true_test():
     assert add_numbers(1,2) == 3
 
-def false_test():
-    assert add_numbers(1,1) == 9
+# def false_test():
+#     assert add_numbers(1,1) == 9
 
 print(true_test())
-print(false_test())
+# print(false_test())
 
