f0a06c4761a2fc6ba55fa01f53792ccc3dc02517
Vedant Patel <104895751+Kiraschild@users.noreply.github.com>
2024-09-10 17:19:34 +0000

    Pylint commit

--- hw2/rand.py
+++ hw2/rand.py
@@ -1,8 +1,10 @@
+""" Module to generate random number to fill the array"""
 import subprocess
 
 def random_array(arr):
+    """ Generating the random array"""
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+    for i, inner_arr in enumerate(arr):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check= False)
+        inner_arr[i] = int(shuffled_num.stdout)
     return arr
