e027b23c3a3e0d53326be2d5dc7fa1da886f5a9b
Smiti Kothari <kotharismiti24@gmail.com>
2024-09-10 18:27:02 +0000

    resolved errors

--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -1,10 +1,10 @@
 from hw2_debugging import merge_sort
 def true_test1():
   arr=[1,5,4,8,2]
-  assert merge_sort(arr)
+  assert merge_sort(arr)==[1,2,4,5,8]
 def true_test2():
-  arr=[2]
-  assert merge_sort(arr)
+  arr=[2] 
+  assert merge_sort(arr)==[2]
 def true_test3():
   arr=[1,4,5,6]
-  assert merge_sort(arr)
+  assert merge_sort(arr)==[1,4,5,6]
