d52266ec23ad244f4a2d1cebafe41425651e7a26
Smiti Kothari <kotharismiti24@gmail.com>
2024-09-10 18:52:44 +0000

    resolving errors

--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -2,9 +2,11 @@ from hw2_debugging import merge_sort
 
 def test_true1():
     arr = [1, 5, 4, 8, 2]
+    expected=[1,2,4,5,8]
     sorted_arr = merge_sort(arr)
     print(f"Test 1 - Output: {sorted_arr}")
-    assert sorted_arr == [1, 2, 4, 5, 8]
+    Assert.Equal(arr,expected)
+  
 
 def test_true2():
     arr = [2]
