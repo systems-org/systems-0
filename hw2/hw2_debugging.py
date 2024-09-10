8cbc62b1ad3fe825ab043ff30feabd39abcc573c
Smiti Kothari <kotharismiti24@gmail.com>
2024-09-10 19:22:34 +0000

    fixed some code

--- hw2/hw2_debugging.py
+++ hw2/hw2_debugging.py
@@ -2,31 +2,39 @@
 import rand
 
 def merge_sort(arr):
-    """ Dividing the array"""
-    if len(arr) == 1:
+    """Dividing the array"""
+    if len(arr) <= 1:
         return arr
 
-    half = len(arr)//2
+    half = len(arr) // 2
 
-    return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
+    left_half = merge_sort(arr[:half])
+    right_half = merge_sort(arr[half:])
+    
+    return recombine(left_half, right_half)
 
 def recombine(left_arr, right_arr):
     """Recombining the array"""
     left_index = 0
     right_index = 0
-    merge_arr = [None] * (len(left_arr) + len(right_arr))
-    while left_index < len(left_arr) and right_index < len(right_arr):
-        if left_arr[left_index] < right_arr[right_index]:
-            right_index += 1
-            merge_arr[left_index + right_index] = left_arr[left_index]
-        else:
-            left_index += 1
-            merge_arr[left_index + right_index] = right_arr[right_index]
+    merge_arr = []
 
-    for i in range(right_index, len(right_arr)):
-        merge_arr[left_index + right_index] = right_arr[i]
-    for i in range(left_index, len(left_arr)):
-        merge_arr[left_index + right_index] = left_arr[i]
+    # Merging the two halves
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] <= right_arr[right_index]:
+            merge_arr.append(left_arr[left_index])
+            left_index += 1
+        else:
+            merge_arr.append(right_arr[right_index])
+            right_index += 1
+
+    # Append remaining elements of left_arr (if any)
+    if left_index < len(left_arr):
+        merge_arr.extend(left_arr[left_index:])
+    
+    # Append remaining elements of right_arr (if any)
+    if right_index < len(right_arr):
+        merge_arr.extend(right_arr[right_index:])
 
     return merge_arr
 
