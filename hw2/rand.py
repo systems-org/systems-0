c26b13fec141b59d89aba98ab4442bbe0edeea14
Jayneel Shah <80264736+jayneel-shah18@users.noreply.github.com>
2024-09-12 20:50:17 +0000

    made changes to rand.py

--- hw2/rand.py
+++ hw2/rand.py
@@ -4,7 +4,7 @@ import subprocess
 def random_array(arr):
     """ Generating the random array"""
     shuffled_num = None
-    for i, inner_arr in enumerate(arr):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check= False)
-        inner_arr[i] = int(shuffled_num.stdout)
+    for inner_arr in arr:
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
+        inner_arr[0] = int(shuffled_num.stdout.decode().strip())  
     return arr
